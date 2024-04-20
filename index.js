194db4654fec4dac4881a592fda31e279e7a8438
DJVMEX <djvmex@gmail.com>
2024-04-20 13:45:33 -0300

    Segunda Entrega

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
+const fs = require('fs');
+
 class ProductManager {
     #priceBaseGain = 0.15;
 
-    constructor() {
-        this.products = [];
+    constructor(path) {
+        this.path = path;
     }
 
-    getProducts() {
-        return this.products;
+    async getProducts() 
+    {
+        try
+        {
+            if(fs.existsSync(this.path))
+            {
+                const products = await fs.promises.readFile(this.path, 'utf8');
+                return JSON.parse(products);
+            } else return [];
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
     }
-
-    addProduct(title, description, price, thumbnail, code, stock) {
+ 
+    async addProduct(title, description, price, thumbnail, code, stock) 
+    {
         if (!title || !description || !price || !thumbnail || !code || !stock) 
         {
             console.log("Es obligatorio cargar todos los parametros del producto.")
         }
         else
         {
-            if (this.#isCodeExists(code)) {
+            const codeExists = await this.#isCodeExists(code);
+            if (codeExists) {
                 console.log("El codigo Ingresado ya esta cargado.")
             }
             else
             {                
                 const product = {
-                    id: this.#getMaxID() + 1,
+                    id: await this.#getMaxID() + 1,
                     title,
                     description,
                     price,
@@ -30,42 +46,87 @@ class ProductManager {
                     code,
                     stock
                 };
-                this.products.push(product);
+
+                try
+                {
+                    const products = await this.getProducts();
+                    products.push(product);
+                    console.log(products);
+                    await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+
+                }
+                catch(error)
+                {
+                    console.log(error);
+                }
             }
         }
 
 
     }
 
-    #getMaxID() {
-        return this.products.reduce((maxId, product) => Math.max(maxId, product.id), 0);
+    async #getMaxID() 
+    {        
+        try {
+            const products = await this.getProducts();
+            return products.reduce((maxId, product) => Math.max(maxId, product.id), 0);
+        } catch (error) {
+            console.error('Error al verificar la existencia del código:', error);
+            return false; // Asumir que el código no existe si hay un error
+        }
+        return 1;
+
     }
 
-    #isCodeExists(code) {
-        return this.products.some(product => product.code === code);
+    async #isCodeExists(code) 
+    {
+        try 
+        {
+            const products = await this.getProducts();
+            return products.some(product => product.code === code);
+        } catch (error) {
+            console.error('Error linea 86:', error);
+            return false;
+        }
     }
 
 
-    getProductById(idProduct) {
-        return this.products.find(product => product.id === idProduct);
+    async getProductById(idProduct) {
+        try 
+        {
+            const products = await this.getProducts();
+            return products.find(product => product.id === idProduct);
+
+        } catch (error) {
+            console.error('Error linea 86:', error);
+            return false;
+        }
+
     }
 
 }
 
-const productManager = new ProductManager();
+const productManager = new ProductManager('./products.json');
 
-//Alta de Producto
-productManager.addProduct("Coca Cola L 1.5", "Bebida Cola de 1.5 Litros", "15.00", "https://m.media-amazon.com/images/I/51v8nyxSOYL._SL1500_.jpg",10001, 200)
-productManager.addProduct("Pepsi L 2.25", "Bebida Cola de 2.25 Litros", "12.00", "https://www.cdparque.com/img/sections/productos/pepsi.png",10002, 300)
-console.log(productManager.getProducts());
+const test = async() =>{
+    //leo el archivo
+    console.log(await productManager.getProducts())
 
-//Intento repetir el codigo...
-productManager.addProduct("Fanta L 2", "Bebida Naranja de 2 Litros", "10.00", "https://example.com/fanta.png", 10001, 150);
-console.log(productManager.getProducts());
+    //agrego cocacola.
+    await productManager.addProduct("Coca Cola L 1.5", "Bebida Cola de 1.5 Litros", "15.00", "https://m.media-amazon.com/images/I/51v8nyxSOYL._SL1500_.jpg",10001, 200)
 
-// Verifico si sale el error cuando no cargo un parametro.
-productManager.addProduct("Fanta L 2", "Bebida Naranja de 2 Litros", "10.00", "https://example.com/fanta.png", 10001);
+    //agrego pepsi
+    await productManager.addProduct("Pepsi L 2.25", "Bebida Cola de 2.25 Litros", "12.00", "https://www.cdparque.com/img/sections/productos/pepsi.png",10002, 300)
 
-//Busco y Muestro el Producto
-console.log(productManager.getProductById(2));
+    //Intento repetir el codigo...
+    await productManager.addProduct("Fanta L 2", "Bebida Naranja de 2 Litros", "10.00", "https://example.com/fanta.png", 10001, 150);
 
+
+    // Verifico si sale el error cuando no cargo un parametro.
+    await productManager.addProduct("Fanta L 2", "Bebida Naranja de 2 Litros", "10.00", "https://example.com/fanta.png", 10001);
+
+    //Busco y Muestro el Producto
+    console.log(await productManager.getProductById(2));
+}
+
+test();
